1452d8088f450b230c0f90f4a4ae4f5e92cc203f
Allann7 <112590997+Allann7@users.noreply.github.com>
2026-09-09 18:54:15 -0600

    Create functions to get human and computer choice
    created an empty index file and linked a script file with the functions
    mentioned before

---
+++ javascript.js
@@ -0,0 +1,59 @@
+function getComputerChoice(){
+    let CompChoice = Math.floor(Math.random()*100);
+    
+    if (CompChoice <= 33){
+        return "Rock";
+    }
+    else if (CompChoice > 33 & CompChoice < 66){
+        return "Paper";
+    }
+    else {
+        return "Scissor";
+    }
+}
+
+function getHumanChoice(){
+    let HumChoice = prompt(`What will you choose:
+        1. Rock
+        2. Paper
+        3. Scissor`)
+
+        HumChoice = HumChoice.toString();
+        HumChoice = HumChoice.toLowerCase();
+
+        console.log(HumChoice);
+
+        if (HumChoice === "1" || "2" || "3" || "rock" || "paper" || "scissor"){
+            switch (HumChoice){
+                case "1":
+                    return "Rock";
+                break;
+
+                case "2":
+                    return "Paper";
+                break;
+
+                case "3":
+                    return " Scissor";
+                break;
+
+                case "rock":
+                    return "Rock";
+                break;
+
+                case "paper":
+                    return "Paper";
+                break;
+
+                case "scissor":
+                    return "Scissor";
+                break;
+
+                default:
+                    alert("Invalid option, please try again.");
+                    return getHumanChoice();
+            }
+        }
+}
+
+console.log(getHumanChoice());
